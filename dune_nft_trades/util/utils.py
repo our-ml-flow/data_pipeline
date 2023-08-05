6caabf3a9d9929195dc85ae6c78453c856d43342
Datahyeon <reot1004@naver.com>
2023-08-06 01:08:57 +0900

    feat: Add get_dune_api_info function in util.py #3

--- dune_nft_trades/util/utils.py
+++ dune_nft_trades/util/utils.py
@@ -11,24 +11,66 @@ Dependency:
     
 """
 @task(log_prints=True)
-def get_sql_engine(block_name: str) -> sqlalchemy.engine.base.Engine:
+def get_sql_engine(block_name: str) -> sqlalchemy.engine.base.Engine|None:
+    engine = None
+    
     try:
         database_block = SqlAlchemyConnector.load(block_name)
+        
         engine = database_block.get_engine()
+        
         print(f'engine: {engine}')
+        
     except Exception as e:
         print(e.__doc__)
-        engine = None
+        
     finally:
         return engine
 
 @task(log_prints=True)
-def get_json_block(block_name: str, address: str) -> str:
+def get_json_block(block_name: str, key: str) -> str|None:
+    api_key = None
+    
     try:    
         block = JSON.load(block_name)
-        api_key = block.value[address]
+        
+        api_key = block.value[key]
+        
     except Exception as e:
         print(e.__doc__)
-        api_key = None
+        
     finally:
         return api_key
+    
+@task(log_prints=True)
+def get_dune_api_info(engine: sqlalchemy.engine.base.Engine = None) -> pd.DataFrame|None:
+    result = None
+    
+    try:
+        if engine == None:
+            raise ValueError
+    
+    except ValueError:
+        print('No sql engine')
+        
+        return None
+    
+    else:
+        query = """SELECT "address", "usage"
+            FROM dune_nft_info
+            WHERE 2500 - "usage" >= 500
+            ORDER BY 2 DESC
+            LIMIT 1; 
+            """
+            
+        try:
+            dune_api_info_df = pd.read_sql(query, engine)
+        
+        except Exception as e:
+            print('Fail get dune api info')
+            
+        else:
+            result = dune_api_info_df.iloc[0].values
+        
+        finally:
+            return result
