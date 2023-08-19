7fc03d6c73cf87849fae5e65b59cc0e0a5593115
jinlee057 <jinlee057@gmail.com>
2023-08-19 16:10:00 +0900

    feat: Update alchey_daily.py #11
    based on actual data, 2 more flows added.
    load_get_top_seller : get top sellers using query and pass seller list to create_db_collecition_for_seller function
    load_get_top_buyer : same way but for buyer

--- flow/warehouse/alchemy_daily.py
+++ flow/warehouse/alchemy_daily.py
@@ -6,7 +6,8 @@ print(repo_dir)
 sys.path.append(f'{repo_dir}')
 
 from module.utils import get_sql_engine, get_json_block
-from module.warehouse.alchemy.task import create_collection_for_owner,create_owners_for_contract,get_db_data
+from module.warehouse.alchemy.task import create_collection_for_owner,preprocess_collection_for_owner
+from module.warehouse.alchemy.task import get_top_seller, get_top_buyer, load_to_db
 
 from prefect import flow
 from prefect.deployments import Deployment
@@ -15,46 +16,70 @@ from prefect.server.schemas.schedules import CronSchedule
 import time
 from datetime import datetime
 
+from sqlalchemy.dialects.postgresql import JSONB
 
-@flow(name='get_owner')
-def create_db_owners_for_contract():
+
+@flow(name='load_to_db_get_sellers_list', log_prints=True)
+def load_get_top_seller():
+
+    engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
+    df_seller=get_top_seller()
+
+    df_seller.to_sql('top_n_seller_daily',con=engine,if_exists='append',index=False)
+    sellers = df_seller['seller']
+    return sellers
+
+
+@flow(name="get_collection_seller")
+def create_db_collection_for_seller(owners):
     
     engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
-    contracts=['0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D','0xBd3531dA5CF5857e7CfAA92426877b022e612cf8']
-    ALCHEMY_API_KEY=get_json_block('alchemy-api-key','alchemy1')
-    
-    db_owners_for_contract= create_owners_for_contract(contracts,ALCHEMY_API_KEY)
-    start_time=time.time()
-    db_owners_for_contract.to_sql('alchemy_owners_for_contract', con=engine, if_exists='append', index=False)
-    print("owners to_sql duration: {} seconds".format(time.time() - start_time))
-
-
-
-@flow(name="get_collection")
-def create_db_collection_for_owner():
-    
-    engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
-    owners = get_db_data('SELECT DISTINCT "owner" FROM alchemy_owners_for_contract LIMIT 200;')
     ALCHEMY_API_KEY = get_json_block('alchemy-api-key','alchemy1')
 
     db_collection_for_owner = create_collection_for_owner(owners,ALCHEMY_API_KEY)
-    start_time=time.time()
-    db_collection_for_owner.to_sql('alchemy_collection_for_owner', con=engine, if_exists='append', index=False)
-    print("owners to_sql duration: {} seconds".format(time.time() - start_time))
+    db_collection_for_owner = preprocess_collection_for_owner(db_collection_for_owner)
+    db_collection_for_owner.to_sql('alchemy_collection_for_seller', con=engine, if_exists='append', index=False,
+                                   dtype={"opensea_floor_price": JSONB, "contract": JSONB, "display_nft": JSONB, "image": JSONB})
+
+
+@flow(name='load_to_db_get_buyers_list', log_prints=True)
+def load_get_top_buyer():
+
+    engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
+    df_buyer=get_top_buyer()
+    
+    df_buyer.to_sql('top_n_buyer_daily',con=engine,if_exists='append',index=False)
+    buyers = df_buyer['buyer']
+    return buyers
+
+
+@flow(name="get_collection_buyer")
+def create_db_collection_for_buyer(owners):
+    
+    engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
+    ALCHEMY_API_KEY = get_json_block('alchemy-api-key','alchemy1')
+
+    db_collection_for_owner = create_collection_for_owner(owners,ALCHEMY_API_KEY)
+    db_collection_for_owner = preprocess_collection_for_owner(db_collection_for_owner)
+    db_collection_for_owner.to_sql('alchemy_collection_for_buyer', con=engine, if_exists='append', index=False,
+                                   dtype={"opensea_floor_price": JSONB, "contract": JSONB, "display_nft": JSONB, "image": JSONB})
+
 
 
 @flow(name='Alchemy_daily_warehouse_flow', log_prints=True)
 def alchemy_daily():
-    create_db_owners_for_contract()
-    time.sleep(2)
-    create_db_collection_for_owner()
+    sellers = load_get_top_seller()
+    create_db_collection_for_seller(sellers)
+    buyers = load_get_top_buyer()
+    create_db_collection_for_buyer(buyers)
+
 
 if __name__=='__main__':
-
+   
     deployment=Deployment.build_from_flow(
         flow=alchemy_daily,
         name='Alchemy_daily_warehouse_flow_Deployment',
-        version=1.0,
+        version=1.3,
         work_queue_name='alchemy-agent',
         schedule=(CronSchedule(cron="00 11 * * *", timezone="Asia/Seoul"))
     )
