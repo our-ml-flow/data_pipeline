5f6d38f574c799fc72da76c9987029aa835eabe6
jinlee057 <jinlee057@gmail.com>
2023-08-19 16:17:26 +0900

    feat: Update transfor_task.py #11
    update preprocess function to rename column by convention.
    drop unnecessary column 'bannerImageUrl'.
    convert data type for numeric columns.
    add timestamp

---
+++ module/warehouse/alchemy/task/transform_task.py
@@ -0,0 +1,19 @@
+from prefect import task
+import pandas as pd
+
+@task(log_prints=True)
+def preprocess_collection_for_owner(df):
+    new_col_name = {'name':'collection_name','openSeaSlug':'opensea_slug','openSea':'opensea_floor_price',
+                    'externalUrl':'external_url','twitterUsername':'twitter_username','discordUrl':'discord_url',
+                    'totalBalance':'total_balance','numDistinctTokensOwned':'num_distinct_tokens_owned',
+                    'isSpam':'is_spam','displayNft':'display_nft'}
+
+    df.rename(columns=new_col_name, inplace=True)
+    df.drop(columns=['bannerImageUrl'], inplace=True)
+
+    df['total_balance']=pd.to_numeric(df['total_balance'], errors='coerce')
+    df['num_distinct_tokens_owned']=pd.to_numeric(df['num_distinct_tokens_owned'], errors='coerce')
+    
+    df['data_created_at'] = pd.Timestamp.utcnow().isoformat()
+
+    return df
