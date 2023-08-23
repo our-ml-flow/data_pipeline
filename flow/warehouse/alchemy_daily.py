07da602a5cafe3e08565479feaf9d800390f78f3
jinlee057 <jinlee057@gmail.com>
2023-08-23 16:40:25 +0900

    feat: Update alchemy_daily flow #11
    get nft colleciton infomation using alchemy API. and load to DB.

--- flow/warehouse/alchemy_daily.py
+++ flow/warehouse/alchemy_daily.py
@@ -7,7 +7,7 @@ sys.path.append(f'{repo_dir}')
 
 from module.utils import get_sql_engine, get_json_block
 from module.warehouse.alchemy.task import create_collection_for_owner,preprocess_collection_for_owner
-from module.warehouse.alchemy.task import get_top_seller, get_top_buyer, load_to_db
+from module.warehouse.alchemy.task import get_top_seller_list,get_top_buyer_list
 
 from prefect import flow
 from prefect.deployments import Deployment
@@ -19,16 +19,6 @@ from datetime import datetime
 from sqlalchemy.dialects.postgresql import JSONB
 
 
-@flow(name='load_to_db_get_sellers_list', log_prints=True)
-def load_get_top_seller():
-
-    engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
-    df_seller=get_top_seller()
-
-    df_seller.to_sql('top_n_seller_daily',con=engine,if_exists='append',index=False)
-    sellers = df_seller['seller']
-    return sellers
-
 
 @flow(name="get_collection_seller")
 def create_db_collection_for_seller(owners):
@@ -38,19 +28,10 @@ def create_db_collection_for_seller(owners):
 
     db_collection_for_owner = create_collection_for_owner(owners,ALCHEMY_API_KEY)
     db_collection_for_owner = preprocess_collection_for_owner(db_collection_for_owner)
+  
     db_collection_for_owner.to_sql('alchemy_collection_for_seller', con=engine, if_exists='append', index=False,
                                    dtype={"opensea_floor_price": JSONB, "contract": JSONB, "display_nft": JSONB, "image": JSONB})
 
-
-@flow(name='load_to_db_get_buyers_list', log_prints=True)
-def load_get_top_buyer():
-
-    engine = get_sql_engine('gcp-mlops-sql-postgres').get_engine()
-    df_buyer=get_top_buyer()
-    
-    df_buyer.to_sql('top_n_buyer_daily',con=engine,if_exists='append',index=False)
-    buyers = df_buyer['buyer']
-    return buyers
 
 
 @flow(name="get_collection_buyer")
@@ -68,9 +49,9 @@ def create_db_collection_for_buyer(owners):
 
 @flow(name='Alchemy_daily_warehouse_flow', log_prints=True)
 def alchemy_daily():
-    sellers = load_get_top_seller()
+    sellers = get_top_seller_list()
     create_db_collection_for_seller(sellers)
-    buyers = load_get_top_buyer()
+    buyers = get_top_buyer_list()
     create_db_collection_for_buyer(buyers)
 
 
