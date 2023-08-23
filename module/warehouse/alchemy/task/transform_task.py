c9992119f428c4c824c3dfbace4ab8ed4db5fb74
jinlee057 <jinlee057@gmail.com>
2023-08-23 16:57:14 +0900

    feat: Update transfform_task.py #11
    amend data_created_at. same date with duen query date instead of today

--- module/warehouse/alchemy/task/transform_task.py
+++ module/warehouse/alchemy/task/transform_task.py
@@ -1,8 +1,12 @@
 from prefect import task
 import pandas as pd
+from datetime import datetime,timedelta
 
 @task(log_prints=True)
 def preprocess_collection_for_owner(df):
+    current_date = datetime.today()
+    target_date = current_date - timedelta(days=2)
+
     new_col_name = {'name':'collection_name','openSeaSlug':'opensea_slug','openSea':'opensea_floor_price',
                     'externalUrl':'external_url','twitterUsername':'twitter_username','discordUrl':'discord_url',
                     'totalBalance':'total_balance','numDistinctTokensOwned':'num_distinct_tokens_owned',
@@ -14,6 +18,6 @@ def preprocess_collection_for_owner(df):
     df['total_balance']=pd.to_numeric(df['total_balance'], errors='coerce')
     df['num_distinct_tokens_owned']=pd.to_numeric(df['num_distinct_tokens_owned'], errors='coerce')
     
-    df['data_created_at'] = pd.Timestamp.utcnow().isoformat()
-
+    df['data_created_at'] = target_date
+    
     return df
