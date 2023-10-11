c7732c2ea2df03770cab5897e1299194fbeca51b
Datahyeon <reot1004@naver.com>
2023-10-11 13:49:01 +0900

    feat: Add new util function and refactor dune pipeline #28

--- flow/warehouse/dune_daily.py
+++ flow/warehouse/dune_daily.py
@@ -5,75 +5,104 @@ repo_dir = os.path.abspath(__file__).split('/flow')[0]
 print(repo_dir)
 sys.path.append(f'{repo_dir}')
 
-from module.utils import get_sql_engine, get_json_block, get_dune_api_info, log_dune_datapoint, update_dune_api_info_usage
+from module.utils import get_sql_engine, get_json_block, get_dune_api_info, log_dune_datapoint, update_dune_api_info_usage, telegram_msg_send
 from module.warehouse.dune_daily.task import get_dune_nft_trade, extract_dune_trades_value, transform_drop_na, reset_columns, load_dune_nft_trade
 from prefect.deployments import Deployment
 from prefect.server.schemas.schedules import CronSchedule
+from prefect.blocks.system import JSON
 from prefect import flow
 from datetime import datetime, timedelta
 
-@flow(name='Initialize_setting_flow', log_prints=True)
-def init_sql_setting(sql_block_name: str, json_block_name: str) -> tuple:
-    database_block = get_sql_engine(sql_block_name)
-    
-    engine = database_block.get_engine()
-    
-    api_address, api_usage = get_dune_api_info(engine)
-    
-    api_key = get_json_block(json_block_name, api_address)
-    
-    return (engine, api_address, api_usage, api_key)
+import traceback
 
-@flow(name='ETL_flow', log_prints=True)
-def dune_etl_flow(init_info: tuple) -> tuple|None:
-    engine, api_address, api_usage, api_key = init_info
-    
-    response = get_dune_nft_trade(api_key)
-    
-    datapoint = log_dune_datapoint(response)
-    
-    extract_date = datetime.strftime(datetime.today() - timedelta(days=1), '%Y.%m.%d')
-    
-    if datapoint == None:
-        
-        print(f'{extract_date} required datapoint is more than {api_usage}')
-        
-        print('Fail ETL_flow')
-        
-        return None
-    
-    else:
-        print(f'{extract_date} required datapoint is {datapoint}')
-        
-        print(f'Before usage: {api_usage}, After usage: {api_usage+datapoint}')
-    
-    dune_df = extract_dune_trades_value(response)
-    
-    dune_df = transform_drop_na(dune_df)
-    
-    dune_df = reset_columns(dune_df)
-    
-    load_dune_nft_trade(engine, dune_df)
-
-    update_dune_api_info_usage(engine, api_address, api_usage, datapoint)
-    
 @flow(name='Dune_daily_warehouse_flow', log_prints=True)
 def dune_nft_trades():
+    json_block = JSON.load('telegram')
+    
+    bot_token, chat_id = json_block.value["mlops_bot"].values()
+    
+    start_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+    
+    start_msg = '\n'.join(['Dune Pipeline Start', start_time])
+    
+    telegram_msg_send(bot_token, chat_id, start_msg)
+    
     sql_block_name = 'gcp-mlops-sql-postgres'
     
-    json_block_name = 'dune-api-key'
+    dune_json_block_name = 'dune-api-key'
     
-    init_info = init_sql_setting(sql_block_name, json_block_name)
+    try:
+        sql_block = get_sql_engine(sql_block_name)
+        
+        engine = sql_block.get_engine()
+        
+        dune_api_table = get_dune_api_info(engine)
+        
+        dune_api_trial = 1
+        for label, row in dune_api_table.iterrows():
+            if dune_api_trial > 3:
+                raise ValueError
+            
+            api_id, owner, usage = row
+            
+            dune_api_key = get_json_block(dune_json_block_name, api_id)
+            
+            response = get_dune_nft_trade(dune_api_key)
+            
+            if response == None:
+                dune_api_trial += 1
+                continue
+            
+            else:
+                break
+            
+        dune_data_point = log_dune_datapoint(response)
+        
+        dune_df = extract_dune_trades_value(response)
+        
+        dune_df, before_len, after_len = transform_drop_na(dune_df)
+        
+        dune_df = reset_columns(dune_df)
+        
+        load_dune_nft_trade(engine, dune_df)
+        
+        after_usage = update_dune_api_info_usage(engine, api_id, usage, dune_data_point)    
     
-    dune_etl_flow(init_info)
+    except Exception as e:
+        error_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+        
+        error_header = e.__doc__
+        
+        error_body = str(e)
+        
+        error_trace = traceback.format_exc()
+        
+        error_msg = '\n'.join(['Dune Pipeline Error', error_time, '',
+                            error_header, '', error_body, '', error_trace])
+        
+        telegram_msg_send(bot_token, chat_id, error_msg)
+        
+        raise 
+
+    else:
+        end_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+        
+        end_msg = '\n'.join(['Dune Pipeline End', end_time, '',
+                            f'api_id: {api_id}',
+                            f'owner: {owner}',
+                            f'before_usage: {usage}',
+                            f'after_usage: {after_usage}',
+                            f'today_data_length: tr_before - {before_len}, tr_after - {after_len}'])
+
+        telegram_msg_send(bot_token, chat_id, end_msg)
 
 if __name__=="__main__":
     deployment = Deployment.build_from_flow(
         flow=dune_nft_trades,
-        name="Dune nft trades Flow Deployment",
-        version=1.0,
+        name="LSH: Test Dune Nft Trades Flow Deployment",
+        version='Test',
         work_queue_name="dune-agent",
-        schedule=(CronSchedule(cron="30 10 * * *", timezone="Asia/Seoul"))
+        schedule=(CronSchedule(cron="40 09 * * *", timezone="Asia/Seoul"))
     )
     
     deployment.apply()
--- module/warehouse/dune_daily/task/extract_task.py
+++ module/warehouse/dune_daily/task/extract_task.py
@@ -4,67 +4,46 @@ from prefect import task
 import pandas as pd
 
 @task(log_prints=True)
-def get_dune_nft_trade(api_key: str = None) -> Response|None:
-    response = None
+def get_dune_nft_trade(api_key: str) -> Response|None:
+    query_id = 2759265
+    
+    url = f"https://api.dune.com/api/v1/query/{query_id}/results?api_key={api_key}"
+    
+    response = get(url)
     
     try:
-        if api_key == None:
-            raise ValueError
+        if response.status_code != 200:
+            raise ConnectionError
         
-    except ValueError:
-        print("No dune api key")
+    except ConnectionError:
+        print("No dune response 200")
         
+        response = None
+    
+    except Exception as e:
+        print(e.__doc__)
+        
+        response = None
+    
     else:
-        query_id = 2759265
-        
-        url = f"https://api.dune.com/api/v1/query/{query_id}/results?api_key={api_key}"
-        
-        response = get(url)
-        
-        try:
-            if response.status_code != 200:
-                raise ConnectionError
-            
-        except ConnectionError:
-            print("No dune response 200")
-            
-            response = None
-        
-        except Exception as e:
-            print(e.__doc__)
-            
-            response = None
-        
-        else:
-            print("Dune response 200")
-
+        print("Dune response 200")
+    
     finally:
         return response
 
 @task(log_prints=True)
-def extract_dune_trades_value(response: Response = None) -> pd.DataFrame:
-    dune_nft_trades_df = None
-    
+def extract_dune_trades_value(response: Response) -> pd.DataFrame:
     try:
-        if response == None:
-            raise ValueError
+        req_json = response.json()
         
-    except ValueError:
-        print("Response value error")
+        rows = req_json["result"]["rows"]
+        
+    except Exception as e:
+        print(e.__doc__)
+        
+        raise
         
     else:
-        try:
-            req_json = response.json()
-            
-            rows = req_json["result"]["rows"]
-            
-        except Exception as e:
-            print(e.__doc__)
-            
-        else:
-            dune_nft_trades_df = pd.DataFrame(rows)
-            
-            print("Success parsing json")
+        dune_nft_trades_df = pd.DataFrame(rows)
         
-    finally:    
-        return dune_nft_trades_df
+        return dune_nft_trades_df    
